3d62b78b9dd3fa015976c1379993c1205771c26e
JianweiZheng <jianweizheng@pku.edu.cn>
2019-11-18 17:12:02 +0800

    build the homepage

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,25 @@
 <template>
     <div class="home">
         <nav-bar class="nav-bar"></nav-bar>
+        <div class="account-content">
+            <div>
+                <img class="token-icon"
+                     src="../../static/icons/ic_v_logo@3x.png">
+            </div>
+            <div class="balance">
+                <span class="token-balance">3097.7997</span>
+                <span class="unit">VSYS</span>
+            </div>
+            <div class="btn">
+                <b-button class="btn-deposit">
+                    <img class="icon-btn"
+                         src="../../static/icons/ic_deposit@3x.png"><b class="deposit-txt">Deposit</b></b-button>
+                <b-button class="btn-send">
+                    <img class="icon-btn"
+                         src="../../static/icons/ic_send@3x.png"><b class="send-txt">Send</b></b-button>
+            </div>
+        </div>
+        <transaction-records class="transaction-records"></transaction-records>
     </div>
 </template>
 
@@ -30,7 +49,89 @@ export default {
 .nav-bar {
     height: 60px;
     width: 100%;
-    position: fixed;
     z-index:1000;
+}
+.account-content {
+    height: 237px;
+    width: 100%;
+}
+.token-icon {
+    width: 56px;
+    height: 56px;
+    margin-left:152px;
+    margin-top:32px;
+}
+.balance {
+    margin-top: 20px;
+    width: 100%;
+    height: 33px;
+}
+.token-balance {
+    display:inline-block;
+    font-size:28px;
+    font-family:Roboto-Regular,Roboto;
+    font-weight:400;
+    color:rgba(50,50,51,1);
+    line-height:33px;
+    margin-left: 93px;
+}
+.unit {
+    display:inline-block;
+    font-size:16px;
+    font-family:Roboto-Regular,Roboto;
+    font-weight:400;
+    color:rgba(50,50,51,1);
+    line-height:19px;
+    margin-left: 4px;
+}
+.btn {
+    width:100%;
+    height:96px;
+}
+.btn-deposit {
+    margin-top: 24px;
+    display:inline-block;
+    width:128px;
+    height:40px;
+    background:rgba(0,188,37,1);
+    border-radius:4px;
+}
+.btn-send {
+    margin-top: 24px;
+    margin-left: 20px;
+    width:128px;
+    height:40px;
+    background:rgba(246,0,46,1);
+    border-radius:4px;
+}
+.icon-btn {
+    width:16px;
+    height:16px;
+}
+.deposit-txt {
+    width:59px;
+    height:19px;
+    font-size:16px;
+    font-family:SFProText-Regular,SFProText;
+    font-weight:400;
+    color:rgba(255,255,255,1);
+    line-height:19px;
+    margin-left:8px;
+}
+.send-txt {
+    width:39px;
+    height:19px;
+    font-size:16px;
+    font-family:SFProText-Regular,SFProText;
+    font-weight:400;
+    color:rgba(255,255,255,1);
+    line-height:19px;
+    margin-left:8px;
+}
+.transaction-records {
+    height: 263px;
+    width: 328px;
+    z-index:1000;
+    margin-left:16px;
 }
 </style>
---
+++ src/components/TransactionRecord.vue
@@ -0,0 +1,101 @@
+<template>
+    <b-container class="record-unit">
+        <b-row>
+            <b-col class="record-icon"
+                   cols="auto">
+                <img src="../../static/icons/ic_sent@3x.png"
+                     width="32px"
+                     height="32px">
+            </b-col>
+            <b-col class="record-detail-1"
+                   col="auto"
+                   align-v="left">
+                <b-row>
+                    <b-col class="record-type">Send VSYS</b-col>
+                </b-row>
+                <b-row>
+                    <b-col class="record-time">10/21/2019 at 18:32</b-col>
+                </b-row>
+            </b-col>
+            <b-col class="record-detail-2"
+                   col="auto"
+                   align-v="right">
+                <b-row>
+                    <b-col class="record-amount">-800 VSYS</b-col>
+                </b-row>
+                <b-row>
+                    <b-col><span class="record-status">CONDIRMED</span></b-col>
+                </b-row>
+            </b-col>
+        </b-row>
+    </b-container>
+</template>
+
+<script>
+    export default {
+        name: "TransactionRecord"
+    }
+</script>
+
+<style>
+.record-unit {
+    background:rgba(255,255,255,1);
+}
+.record-icon {
+    margin-left: 12px;
+    margin-top: 16px;
+    display:inline-block;
+}
+.record-detail-1 {
+    height: 64px;
+    display:inline-block;
+    text-align:left;
+    border-bottom:1px solid rgba(240,240,245,1);
+}
+.record-type {
+    margin-left: -15px;
+    margin-top: 16px;
+    font-size:14px;
+    font-family:SFProText-Regular,SFProText;
+    font-weight:400;
+    color:rgba(50,50,51,1);
+    line-height:16px;
+}
+.record-time {
+    margin-left: -15px;
+    margin-top: 3px;
+    font-size:11px;
+    font-family:Roboto-Regular,Roboto;
+    font-weight:400;
+    color:rgba(169,169,176,1);
+    line-height:13px;
+    width: 102px;
+    margin-right: 0px;
+    white-space: nowrap;
+}
+.record-detail-2 {
+    height: 64px;
+    text-align:right;
+    border-bottom:1px solid rgba(240,240,245,1);
+}
+.record-amount {
+    margin-top: 16px;
+    font-size:14px;
+    font-family:Roboto-Regular,Roboto;
+    font-weight:400;
+    color:rgba(50,50,51,1);
+    line-height:16px;
+}
+.record-status {
+    margin-top: 3px;
+    font-size:10px;
+    font-family:SFProText-Regular,SFProText;
+    font-weight:400;
+    color:rgba(113,145,0,1);
+    line-height:12px;
+    background-size:69px 16px;
+    background:rgba(235,250,215,1);
+    border-radius:2px;
+    padding: 3px 3px;
+}
+</style>
