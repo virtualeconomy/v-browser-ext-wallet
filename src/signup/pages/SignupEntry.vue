7795d9da658cf2342ffc5e3f713b310a4fd93ba5
Haynar216 <1500012913@pku.edu.cn>
2019-11-22 17:56:52 +0800

    adjust signupEntry style

--- src/signup/pages/SignupEntry.vue
+++ src/signup/pages/SignupEntry.vue
@@ -10,8 +10,8 @@
             <div class="import-part">
                 <div><img class="import-icon"
                           src="../../../static/icons/ic_import_big@2x.png"></div>
-                <div><span class="txt-1">No, I already have a seed phrase</span></div>
-                <div><span class="txt-2">Import your existing wallet using a 12 word seed phrase</span></div>
+                <div style="margin-top: 23px; text-align: left !important;"><span class="txt-1">No, I already have a seed phrase</span></div>
+                <div style="margin-top: 5px;"><span class="txt-2">Import your existing wallet using a 12 word seed phrase</span></div>
                 <div>
                     <b-button type="button" class="btn-import btn-warning"
                               @click="changePage('/signup')">
@@ -21,8 +21,8 @@
             <div class="create-part">
                 <div><img class="create-icon"
                           src="../../../static/icons/ic_add_account@2x.png"></div>
-                <div><span class="txt-1">Yes, let's get set up!</span></div>
-                <div><span class="txt-2">This will create a new wallet and seed phrase</span></div>
+                <div style="margin-top: 23px;"><span class="txt-1">Yes, let's get set up!</span></div>
+                <div style="margin-top: 5px;"><span class="txt-2">This will create a new wallet and seed phrase</span></div>
                 <div>
                     <b-button type="button" class="btn-create btn-warning"
                               @click="changePage('/create')">
@@ -68,7 +68,7 @@ export default {
 }
 .new-part {
     width:1680px;
-    height:226px;
+    height:145px;
 }
 .new-txt {
     margin-left:720px;
@@ -80,6 +80,7 @@ export default {
     line-height:40px;
 }
 .import-part {
+    padding: 41px 47px;
     width:397px;
     height:288px;
     background:rgba(255,255,255,1);
@@ -92,7 +93,6 @@ export default {
 .import-icon {
     width:33px;
     height:33px;
-    margin-top:47px;
 }
 .txt-1 {
     font-size:19px;
@@ -100,7 +100,6 @@ export default {
     font-weight:600;
     color:rgba(50,50,51,1);
     line-height:22px;
-    margin-top: 23px;
 }
 .txt-2 {
     font-size:15px;
@@ -116,7 +115,7 @@ export default {
     height:47px;
     background:rgba(255,136,55,1);
     border-radius:5px;
-    margin-top: 42px;
+    margin-top: 23px;
     border-color: #FF8737
 }
 .btn-warning {
@@ -133,6 +132,7 @@ export default {
 .create-part {
     width:397px;
     height:288px;
+    padding: 41px 47px;
     background:rgba(255,255,255,1);
     border-radius:7px;
     border:1px solid rgba(230,230,237,1);
@@ -143,14 +143,13 @@ export default {
 .create-icon {
     width:33px;
     height:33px;
-    margin-top:47px;
 }
 .btn-create {
     width:187px;
     height:47px;
     background:rgba(255,136,55,1);
     border-radius:5px;
-    margin-top: 42px;
+    margin-top: 23px;
     border-color: #FF8737
 }
 .create-txt {
