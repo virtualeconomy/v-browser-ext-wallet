98d18aca79808ba66cbb3a9140d7499ed215dd59
Haynar216 <32302909+Haynar216@users.noreply.github.com>
2021-10-25 17:30:38 +0800

    add XGO token (#32)

--- src/utils/certify.js
+++ src/utils/certify.js
@@ -5,7 +5,8 @@ const certifiedTokensList = {
         'TWZZfKFqcaNVe5TrphLRNEm5DQFnBRJMjDDByqv84': { name: 'IPX', iconUrl: '../../static/icons/token/IPX.svg', contractType: 'TokenContract' },
         'TWaN4DqnRMdUS5d1ohCn9Vh9VdGWDLm9Um1jHbQiF': { name: 'BlockDesk', iconUrl: '../../static/icons/token/BlockDesk.png', contractType: 'TokenContract' },
         'TWZ3nPYyCJAJJmfJBzxcqe8ZfpYJMQK4dpiQ1m6v4': { name: 'GoldZip', iconUrl: '../../static/icons/token/GoldZip.jpg', contractType: 'TokenContractWithSplit' },
-        'TWar6LKVSYRwxkEZ3Viqa1QAZeq25w93WmHAbppbf': { name: 'OCT', iconUrl: '../../static/icons/token/OCT.jpg', contractType: 'TokenContract' }
+        'TWar6LKVSYRwxkEZ3Viqa1QAZeq25w93WmHAbppbf': { name: 'OCT', iconUrl: '../../static/icons/token/OCT.jpg', contractType: 'TokenContract' },
+        'TWaSLbyVdxTX1C4jPcy56E6iSeRQUkivabcgvPQQU': { name: 'XGO', iconUrl: '../../static/icons/token/XGO.png', contractType: 'TokenContract' }
     },
     Testnet: {
         'TWscu6rbRF2PEsnY1bRky4aKxxKTzn69WMFLFdLxK': { name: 'DM', iconUrl: '../../static/icons/token/DM.svg', contractType: 'TokenContractWithSplit' },
